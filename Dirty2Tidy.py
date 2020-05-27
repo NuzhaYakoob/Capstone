2e56443d11363a2c5954bad4ab36ec7bda12292c
PythonPup <64085316+NuzhaYakoob@users.noreply.github.com>
2020-05-27 06:26:00 -0400

    Add files via upload
    Reference files

---
+++ Dirty2Tidy.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 27 06:19:12 2020
+
+@author: us0nya
+"""
+# Dirty2Tidy - Methods & Reasoning - To generate member_sample_tidy.csv
+
---
+++ FeatureEng_Modeling.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 27 06:21:45 2020
+
+@author: us0nya
+"""
+# FeatureEng_Modeling - To generate AAA Executive Presentation
+
